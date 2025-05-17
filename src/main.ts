ea4ee569c47c6c1c8aab446b7c5f43a8ec7c07ad
chrismoutendy <christophermoutendy@gmail.com>
2025-05-17 20:14:39 +0200

    modifier

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from '../docs/app.config';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
